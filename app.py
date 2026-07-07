b0c937f8281624efacd5ddb8f4f8c3ebc3cd1777
Tannushree Pradhan <TannushreePradhan@email.com>
2026-07-07 23:59:08 +0530

    Fix app.run for Render deployment

--- app.py
+++ app.py
@@ -69,4 +69,4 @@ def predict():
 
 # Run the Flask application
 if __name__ == '__main__':
-    app.run(debug=True) # debug=True enables auto-reload on code changes
+    app.run(host='0.0.0.0' port=5000,debug=False)
